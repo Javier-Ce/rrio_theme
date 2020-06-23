296e9a6c651b254ae7e1c95600a5613d0bf8cdca
Javier Celorrio <javiercelorrio@gmail.com>
2020-06-23 11:55:43 +0200

    reset default them thumbnail

--- functions.php
+++ functions.php
@@ -19,38 +19,7 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
-if ( ! function_exists( 'twentyfifteen_post_thumbnail' ) ) :
-/**
- * Display an optional post thumbnail.
- *
- * Wraps the post thumbnail in an anchor element on index views, or a div
- * element when on single views.
- *
- * @since Twenty Fifteen 1.0
- */
-function twentyfifteen_post_thumbnail() {
-	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
-		return;
-	}
 
-	if ( is_singular() ) :
-	?>
-
-	<div class="post-thumbnail">
-		<?php the_post_thumbnail('large', array( 'alt' => get_the_title() ) ); ?>
-	</div><!-- .post-thumbnail -->
-
-	<?php else : ?>
-
-	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
-		<?php
-			the_post_thumbnail( 'medium', array( 'alt' => get_the_title() ) );
-		?>
-	</a>
-
-	<?php endif; // End is_singular()
-}
-endif;
 
 
 
